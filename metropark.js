fd0c25000123f49e2fc389bb72ce62c4164aa3a0
Sarah Korkis <Sarahkorkis@gmail.com>
2022-11-21 13:17:41 -0500

    base code

---
+++ metropark.js
@@ -0,0 +1,11 @@
+
+function popUp(){
+    var popUp = document.getElementById("popUp");
+
+    if (popUp.style.display === "none"){
+        popUp.style.display = "inline";
+    }
+    else{
+        popUp.style.display = "none";
+    }
+}
